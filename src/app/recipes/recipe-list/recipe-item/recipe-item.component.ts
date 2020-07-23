650d66dc484a140f3dc211d69f6e728cbbe1acf4
Tristanoshier <tsoshier@bsu.edu>
2020-07-23 16:44:05 -0400

    add logic with databinding knowledge

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
+  //Input decorator is used to recieve data while the output component is used to send data out
+  //Seems link Input is used for properties and Output is used for event listeners (EventEmitter) onClick, onChange, etc...
+   @Input() recipe: Recipe;
+   @Output() recipeSelected = new EventEmitter<void>();
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelected() {
+    this.recipeSelected.emit();
   }
 
 }
